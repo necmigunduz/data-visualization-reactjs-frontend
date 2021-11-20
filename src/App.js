20c719d27e4ed7328695a26db06545151ca0e7c1
necmigunduz <necmigunduz@gmail.com>
2021-11-20 11:12:21 +0300

    Add D3 dependency

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React, {useState, useEffect} from 'react'
 import DataFetch from './components/dataFetch';
 
 const App = () => {
-  const [ratings, setRatings] = useState(null);
+  const [data, setData] = useState({});
 
   useEffect(() => {
     DataFetch()
-      .then(result => setRatings(result))
+      .then(result => setData(result))
       .catch(err => console.error(err));
   }, []);
-
-  console.log(ratings)
-
+  console.log(data)
   return (
     <div className='App'>
       <svg></svg>  
