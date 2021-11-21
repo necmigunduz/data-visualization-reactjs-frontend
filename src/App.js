f7ef6e816867e82a5d2db0640f588cee76a9caf0
necmigunduz <necmigunduz@gmail.com>
2021-11-21 21:28:44 +0300

    Add svg and d3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import DataFetch from './components/dataFetch';
+import * as d3 from 'd3';
 
-const App = () => {
+const App = (props) => {
   const [data, setData] = useState({});
-
+  
   useEffect(() => {
     DataFetch()
       .then(result => setData(result))
       .catch(err => console.error(err));
   }, []);
-  console.log(data)
+
+  useEffect(() => {
+    drawGraph(data)
+  },[data])
+
+  function drawGraph() {
+    d3.select('#container')
+      .append('svg')
+      .select('svg')
+      .style('width','600px')
+      .style('height', '606px')
+      .style('background-color', 'gray')
+      .style('display', 'block')
+      .style('margin', 'auto')
+  }
+
   return (
     <div className='App'>
-      <svg></svg>  
+      <div id="container"></div>  
     </div>
   )
 }
